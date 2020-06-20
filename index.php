c697185755c310b6538aebb3787f1a4256fdb07c
marvin menchu <mmenchu@outlook.com>
2020-06-19 22:05:29 -0600

    agregando estructura basica de html para index

--- index.php
+++ index.php
@@ -1,12 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href=css/estilos.css">
-  <title>Agenda</title>
-</head>
-<body>
-  
-</body>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="css/estilos.css">
+    <title>Agenda</title>
+  </head>
+  <body>
+    <div class="contenedor">
+      <h1>Agenda</h1>
+      <div class="contenido">
+        <h2>Agregar nuevo contacto</h2>
+        <form action="crear.php" method="post">
+          <div class="campo">
+            <label for="nombre" >Nombre:
+              <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+            </label>
+          </div><!--- .campo-->
+
+          <div class="campo">
+            <label for="numero">Numero Tel.:
+              <input type="text" name="numero" id="numero" placeholder ="Numero telefónico">
+            </label>
+          </div><!--- .campo-->
+          <input type="submit" value="Agregar">
+        </form>
+      </div><!-- .contenido-->
+    </div><!-- .contenedor-->
+  </body>
 </html>
