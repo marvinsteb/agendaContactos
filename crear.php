7a7ea6c9abbd18bf1fa9a48ac258aefc559394b2
marvin menchu <mmenchu@outlook.com>
2020-06-20 11:56:09 -0600

    creando insert para los campos nombre y numero

--- crear.php
+++ crear.php
@@ -1,12 +1,26 @@
 <?php
- $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : null;
+  $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : null;
   $numero = isset($_POST['numero']) ? $_POST['numero'] : null;
+  
   try {
     require_once('funciones/dbConexion.php');
+    $query = "INSERT INTO `contactos`.`contacto`
+              (`idcontacto`,
+              `nombre`,
+              `telefono`)
+              VALUES
+              (NULL,
+              '{$nombre}',
+              '{$numero}');
+              ";
+
+    $resultado = $conexion->query($query);
+
   } catch (Exception $e) {
     $error = $e->getMessage();
     echo $error;
   }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,10 +34,25 @@
     <div class="contenedor">
       <h1>Agenda</h1>
       <div class="contenido">
+      <?php
+        if($resultado) {
+          echo "Contacto agregado correctamente";
+        } else {
+          echo "Error " . $conexion->error;
+        }
+      ?>
+       <pre>
+       <?php echo $query?>
+       </pre>
        <label for="nombre">Nombre: <?php echo $nombre?></label>
        <br>
        <label for="numero">Numero: <?php echo $numero?></label>
+       <br>
+       <a class='volver' href="index.php">Volver a Inicio</a>
       </div><!-- .contenido-->
     </div><!-- .contenedor-->
+    <?php
+      $conexion->close(); 
+    ?>
   </body>
 </html>
