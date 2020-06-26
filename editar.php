b921ab7265ae242e29451681e6ce2316dbfb4f4d
marvin menchu <mmenchu@outlook.com>
2020-06-25 22:19:38 -0600

    creando query para la pagina editar.php y mostrando los datos en los inputs

--- editar.php
+++ editar.php
@@ -1,10 +1,20 @@
 <?php
+  $idcontacto = isset($_GET['id']) ? $_GET['id'] : null;
   try {
     require_once('funciones/dbConexion.php');
+    $query = "select 
+              ct.idcontacto,
+              ct.nombre,
+              ct.telefono
+              from contacto as ct 
+              where ct.idcontacto = {$idcontacto}";
+   $resultado = $conexion->query($query);
+   $registros = $resultado->fetch_assoc();
   } catch (Exception $e) {
     $error = $e->getMessage();
     echo $error;
   }
+  
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -18,16 +28,17 @@
     <div class="contenedor">
       <h1>Agenda</h1>
       <div class="contenido crear">
+
         <h2>Editar contacto</h2>
         <form action="actualizar.php" method="post">
           <div class="campo">
             <label for="nombre" >Nombre:</label>
-              <input type="text" name="nombre" id="nombre" placeholder="Nombre">
+            <?php echo "<input value ='".$registros['nombre']."'type='text' name='nombre' id='nombre'>";?>
           </div><!--- .campo-->
 
           <div class="campo">
             <label for="numero">Numero Tel.:</label>
-            <input type="text" name="numero" id="numero" placeholder ="Numero telefónico">
+            <?php echo "<input value ='".$registros['telefono']."'type='text' name='numero' id='numero'>";?>
           </div><!--- .campo-->
           <input type="submit" valor="guardar">
         </form>
