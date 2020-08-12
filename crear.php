90be4f4a8f899a495d23ab15bd01bffe8dab2f90
Marvin Menchú <mmenchu@outlook.com>
2020-08-11 23:28:50 -0600

    agregado peticion ajax para crear.php

--- crear.php
+++ crear.php
@@ -1,6 +1,10 @@
 <?php
-  $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : null;
-  $numero = isset($_POST['numero']) ? $_POST['numero'] : null;
+  function peticion_ajax(){
+    return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
+  }
+  
+  $nombre = isset($_POST['nombre']) ? htmlspecialchars($_POST['nombre']) : null;
+  $numero = isset($_POST['numero']) ?  htmlspecialchars($_POST['numero']) : null;
   
   try {
     require_once('funciones/dbConexion.php');
@@ -15,44 +19,16 @@
               ";
 
     $resultado = $conexion->query($query);
+    if(peticion_ajax()) {
+      echo json_encode(array(
+        'respuesta' => $resultado
+      ));
+    } else {
+          exit;
+    }
 
   } catch (Exception $e) {
     $error = $e->getMessage();
-    echo $error;
   }
-
+$conexion->close();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="css/estilos.css">
-    <title>Agenda</title>
-  </head>
-  <body>
-    <div class="contenedor">
-      <h1>Agenda</h1>
-      <div class="contenido">
-      <?php
-        if($resultado) {
-          echo "Contacto agregado correctamente";
-        } else {
-          echo "Error " . $conexion->error;
-        }
-      ?>
-       <pre>
-       <?php echo $query?>
-       </pre>
-       <label for="nombre">Nombre: <?php echo $nombre?></label>
-       <br>
-       <label for="numero">Numero: <?php echo $numero?></label>
-       <br>
-       <a class='volver' href="index.php">Volver a Inicio</a>
-      </div><!-- .contenido-->
-    </div><!-- .contenedor-->
-    <?php
-      $conexion->close(); 
-    ?>
-  </body>
-</html>
--- js/app.js
+++ js/app.js
@@ -1,12 +1,23 @@
 var agregarContacto = document.getElementById("agregar");
-
 var formulario = document.getElementById("formulario_crear_usuario");
+var action = formulario.getAttribute("action");
 
 function crearUsuario() {
   var form_datos = new FormData(formulario);
   for ([llave, valor] of form_datos.entries()) {
     console.log(llave + " " + valor);
   }
+  var xhr = new XMLHttpRequest();
+  xhr.open("post", action, true);
+  xhr.setRequestHeader("X-requested-With", "XMLHttpRequest");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      var resultado = xhr.responseText;
+      console.log("resultado" + resultado);
+      var json = JSON.parse(resultado);
+    }
+  };
+  xhr.send(form_datos);
 }
 
 agregarContacto.addEventListener("click", function (e) {
